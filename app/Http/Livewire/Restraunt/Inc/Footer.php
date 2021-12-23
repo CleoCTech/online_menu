2fe7f0538905a71d1fe7866b1345129e8d64ddae
CleoCTech <cleophasomwenga292@gmail.com>
2021-12-23 17:10:25 +0300

    front-end footer

---
+++ app/Http/Livewire/Restraunt/Inc/Footer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Restraunt\Inc;
+
+use Livewire\Component;
+
+class Footer extends Component
+{
+    public function render()
+    {
+        return view('livewire.restraunt.inc.footer');
+    }
+}
